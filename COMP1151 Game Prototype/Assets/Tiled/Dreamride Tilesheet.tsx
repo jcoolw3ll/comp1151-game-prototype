282699c33f897e305b2b7ac33eb45689c6940fa1
jcoolw3ll <jacob.caldwell@students.mq.edu.au>
2025-06-08 10:43:25 +1000

    Does this work

---
+++ COMP1151 Game Prototype/Assets/Tiled/Dreamride Tilesheet.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Dreamride Tilesheet" tilewidth="16" tileheight="16" tilecount="1036" columns="37">
+ <image source="Tilemap/kenney_roguelike-modern-city/Tilemap/tilemap_packed.png" width="592" height="448"/>
+ <tile id="686">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="2.18182" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="690">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12.8182" y="0" width="2.45455" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="794">
+  <objectgroup draworder="index" id="5">
+   <object id="13" x="13.9091" y="0" width="2" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="795">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="2.18182" height="16"/>
+   <object id="2" x="0" y="0" width="2.18182" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="829">
+  <objectgroup draworder="index" id="3">
+   <object id="5" x="6.09091" y="24"/>
+   <object id="6" x="-8" y="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="830">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.77636e-15" y="13.9091" width="16" height="2.09091"/>
+   <object id="2" x="24" y="3.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="972">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="2.27273" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="974">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.5455" y="0" width="2.45455" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ COMP1151 Game Prototype/Assets/Tiled/DreamrideTilesheetReal.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="DreamrideTilesheet" tilewidth="16" tileheight="16" tilecount="1036" columns="37">
+ <image source="Tilemaps/kenney_roguelike-modern-city/Tilemap/tilemap_packed.png" width="592" height="448"/>
+ <tile id="686">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="3.13043" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="690">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12.9565" y="0" width="3.04348" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="794">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.0435" y="0" width="1.95652" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="795">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="2.08696" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="829">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-8" y="10.0625"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0.25" y="14" width="15.75" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="830">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="14" width="16" height="2"/>
+   <object id="2" x="24" y="9.9375"/>
+   <object id="3" x="7.0625" y="26.3125"/>
+  </objectgroup>
+ </tile>
+ <tile id="972">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="2.17391" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="974">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.8261" y="0" width="2.17391" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
